281c9b35420a2ef380be3fb295d295adbd1f926a
Amanda Vieira <amanda.vieira@voidsoftware.com>
2022-01-04 20:56:36 +0000

    Changed lines to be grid

--- src/components/elements/TimelineContent.tsx
+++ src/components/elements/TimelineContent.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import { EventItem, Group } from '../../types/misc';
 
 interface OwnProps {
@@ -14,89 +14,6 @@ interface OwnProps {
     events: Array<EventItem>;
     eventRenderer?(eventItem: EventItem, style: any): JSX.Element;
 }
-
-class TimelineContent extends Component<OwnProps> {
-    renderColumns = () => {
-        const { columnsSize, headerItemWidth } = this.props;
-        const columns = [];
-        for (let index = 0; index < columnsSize; index++) {
-            columns.push(
-                <div key={`col-${index}`} className="ct-scroll__column" style={{ width: `${headerItemWidth}px` }} />
-            );
-        }
-        return columns;
-    }
-
-    renderEvents = (events: Array<EventItem>) => {
-        // todo - event renderer
-
-        return events.map((event: EventItem, idx: number) => {
-            const { groupId } = event;
-            const style = {
-                width: `${event.width}px`,
-                marginLeft: `${event.left}px`,
-            };
-
-            let overlapPreviousEvent = false;
-            if (idx > 0 && events.length > 1) {
-                const previousEvent = events[idx - 1];
-                console.log(previousEvent);
-                const diff = event.startPeriod.getTime() - previousEvent.endPeriod.getTime();
-                console.log("----", diff);
-                overlapPreviousEvent = diff < 0;
-            }
-
-            if (overlapPreviousEvent) {
-                const eventsWrapperEl: HTMLDivElement = document.querySelector(`[data-events-wrapper="${groupId}"]`) as HTMLDivElement;
-                const lineEl: HTMLDivElement = document.querySelector(`[data-line-groupid="${groupId}"]`) as HTMLDivElement;
-                const groupSidebarEl: HTMLDivElement = document.querySelector(`[data-sidebar-item="${groupId}"]`) as HTMLDivElement;
-                if (eventsWrapperEl && lineEl) {
-                    lineEl.style.height = `${eventsWrapperEl.offsetHeight}px`;
-                    groupSidebarEl.style.height = `${eventsWrapperEl.offsetHeight}px`;
-                }
-            }
-
-            return (
-                <div
-                    key={event.id}
-                    className="ct-event"
-                    style={{
-                        ...style,
-                        backgroundColor: event.backgroundColor,
-                        top: overlapPreviousEvent ? '20px' : '0px',
-                    }}
-                >
-                    {event.title}
-                </div>
-            );
-        })
-    }
-
-    render() {
-        const { groups, events } = this.props;
-
-        return (
-            <div className="ct-scroll">
-                {groups.map((group: Group, idx: number) => {
-                    const groupEvents = events.filter(e => e.groupId === group.id);
-                    return (
-                        <div key={idx} className="ct-scroll__line" data-line-groupid={group.id}>
-                            {this.renderColumns()}
-                            <div className="ct-scroll__line__events" data-events-wrapper={group.id}>
-                                {this.renderEvents(groupEvents)}
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-}
-
-export default TimelineContent;
-
-/*
-import { FunctionComponent } from 'react';
 
 interface OwnProps {
     groups: Array<Group>;
@@ -108,78 +25,73 @@ interface OwnProps {
 
 const TimelineContent: FunctionComponent<OwnProps> = (props: OwnProps) => {
     const {
-        groups, columnsSize, headerItemWidth, events, eventRenderer,
+        events, groups, headerItemWidth, columnsSize, eventRenderer,
     } = props;
 
-    const renderColumns = () => {
-        const columns = [];
-        for (let index = 0; index < columnsSize; index++) {
-            columns.push(
-                <div key={`col-${index}`} className="ct-scroll__column" style={{ width: `${headerItemWidth}px` }} />
-            );
-        }
-        return columns;
-    }
+    const renderEvents = (events: Array<EventItem>) => {
+        // todo - event renderer
 
-    const renderLinesAndColumns = () => {
-        const lines = [];
-        for (let index = 0; index < groups.length; index++) {
-            lines.push(
-                <div className="ct-scroll__line" key={`line-${index}`} data-line-groupid={groups[index].id}>
-                    { renderColumns() }
-                    { renderEvents(groups[index].id) }
+        return events.map((event: EventItem, idx: number) => {
+            // @ts-ignore
+            const columnStart = (event.left / headerItemWidth) - 1;
+            // @ts-ignore
+            const columnEnd = columnStart + (event.width / headerItemWidth);
+
+            let overlapPreviousEvent = false;
+            if (idx > 0 && events.length > 1) {
+                const previousEvent = events[idx - 1];
+                const diff = event.startPeriod.getTime() - previousEvent.endPeriod.getTime();
+                overlapPreviousEvent = diff < 0;
+            }
+
+            const style = {
+                width: `${event.width}px`,
+                marginLeft: `${event.left}px`,
+                gridColumn: columnStart / columnEnd,
+                backgroundColor: event.backgroundColor,
+                gridRow: overlapPreviousEvent ? '2 / 3' : '1 / 2',
+            };
+
+            if (eventRenderer) return eventRenderer(event, style);
+
+            return (
+                <div
+                    key={event.id}
+                    className="ct-event"
+                    style={style}
+                >
+                    {event.title}
                 </div>
             );
-        }
-        return lines;
-    }
-
-    const renderEvents = (groupId: number | string) => {
-        const groupEvents = events.filter(e => e.groupId === groupId);
-        const eventsEl = [];
-
-        for (let index = 0; index < groupEvents.length; index++) {
-            const style = {
-                width: `${groupEvents[index].width}px`,
-                marginLeft: `${groupEvents[index].left}px`,
-            };
-            if (eventRenderer) {
-                eventsEl.push(eventRenderer(groupEvents[index], style));
-            } else {
-                eventsEl.push(
-                    <div
-                        className="ct-event"
-                        style={{
-                            ...style,
-                            backgroundColor: groupEvents[index].backgroundColor,
-                        }}
-                    >
-                        {groupEvents[index].title}
-                    </div>
-                )
-            }
-        }
-
-        // if (eventsEl.length > 1) {
-        //     const eventsWrapperEl: HTMLDivElement = document.querySelector(`[data-events-wrapper="${groupId}"]`) as HTMLDivElement;
-        //     const lineEl: HTMLDivElement = document.querySelector(`[data-line-groupid="${groupId}"]`) as HTMLDivElement;
-        //     const groupSidebarEl: HTMLDivElement = document.querySelector(`[data-sidebar-item="${groupId}"]`) as HTMLDivElement;
-        //     if (eventsWrapperEl && lineEl) {
-        //         lineEl.style.height = `${eventsWrapperEl.offsetHeight}px`;
-        //         groupSidebarEl.style.height = `${eventsWrapperEl.offsetHeight}px`;
-        //     }
-        // }
-        return eventsEl;
+        })
     }
 
     return (
-        <div className="ct-scroll" style={{
-            //width: `calc(100% - ${sidebarWidth}px)`
-        }}>
-            { renderLinesAndColumns()}
+        <div className="ct-scroll">
+            {groups.map((group: Group, idx: number) => {
+                const groupEvents = events.filter(e => e.groupId === group.id);
+                const sidebarItem = document.querySelector(`[data-sidebar-item="${group.id}"]`);
+                let minHeight = 10;
+                if (sidebarItem) {
+                    minHeight = sidebarItem.getBoundingClientRect().height;
+                }
+
+                return (
+                    <div
+                        key={idx}
+                        className="ct-scroll__line"
+                        data-line-groupid={group.id}
+                        style={{
+                            gridTemplateColumns: `repeat(${columnsSize}, ${headerItemWidth}px)`,
+                            minHeight: `${minHeight}px`,
+                        }}
+                    >
+                        {renderEvents(groupEvents)}
+                    </div>
+                );
+            })}
         </div>
     );
 }
 
 export default TimelineContent;
-*/
